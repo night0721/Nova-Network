81fbf8a117ec4d9ec13d8fd095e95b1060edea5b
night0721 <oscarfu0721@gmail.com>
2022-07-27 23:55:31 +0100

    Initial commit

---
+++ src/main/java/me/night0721/network/Network.java
@@ -0,0 +1,11 @@
+package me.night0721.network;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Network extends Plugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("[NETWORK] Success");
+    }
+}
